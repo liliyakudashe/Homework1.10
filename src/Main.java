8912b7807de0deddd98351548162a5fecda124ec
Acer <oduvan4ik_888@mail.ru>
2022-09-21 11:34:01 +0300

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        System.out.println("Первая задача");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        System.out.println(lastName+" "+firstName+" "+middleName);
+
+        // Задача 2
+        System.out.println("Вторая задача");
+        String fullName = lastName+" "+firstName+" "+middleName;
+        fullName = fullName.toUpperCase();
+        System.out.println(fullName);
+
+        // Задача 3
+        System.out.println("Третяя задача");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
+
 }
